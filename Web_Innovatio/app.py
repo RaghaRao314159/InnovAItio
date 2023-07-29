e8459c16b1be678d24da72e5952a57ce03bb3176
Ragha Rao <ragharao2001@gmail.com>
2023-07-30 04:35:05 +0800

    added convo

--- Web_Innovatio/app.py
+++ Web_Innovatio/app.py
@@ -35,10 +35,39 @@ def chatpage():
         redirect(url_for('contactpage'))
 
     return render_template('chatpage.html')
-    
 
+    
+@app.route('/ragha',methods=['POST','GET'])
+def ragha():
+    if request.method == 'POST':
+        ragha_message = request.form.get('ragha_message')
+        global ragha_convo
+        ragha_convo += "Ragha: " + ragha_message + "\n" + "\n"
+        return render_template('ragha.html', ragha_convo = ragha_convo, raksha_convo = raksha_convo)
+    
+    if request.method == 'GET':
+        return render_template('ragha.html', ragha_convo = ragha_convo, raksha_convo = raksha_convo)
+
+    return render_template('ragha.html')
+
+
+
+@app.route('/raksha',methods=['POST','GET'])
+def raksha():
+    if request.method == 'POST':
+        raksha_message = request.form.get('raksha_message')
+        global raksha_convo
+        raksha_convo += "Raksha: " + raksha_message + "\n" + "\n"
+
+        return render_template('raksha.html', raksha_convo = raksha_convo, ragha_convo = ragha_convo)
+    
+    if request.method == 'GET':
+        return render_template('raksha.html', ragha_convo = ragha_convo, raksha_convo = raksha_convo)
+
+    return render_template('raksha.html')
+    
 
 if __name__ == "__main__":
+    ragha_convo, raksha_convo = "", ""
     app.run(debug=True)
-    
     
