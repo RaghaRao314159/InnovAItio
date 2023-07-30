e90d42305168bef9caef9b7fe28d5909b65b92dd
Ragha Rao <ragharao2001@gmail.com>
2023-07-30 21:43:43 +0800

    chat enables

--- Web_Innovatio/app.py
+++ Web_Innovatio/app.py
@@ -1,13 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for
+import random
+import sched, time
 
 app = Flask(__name__)
 
 @app.route('/',methods=['POST','GET'])
 def contactpage():
     if request.method == 'POST':
-        #global bool1
-        #bool1 = (lambda x: "CORRECT" if (request.form.get('stage1') == "Initial:a_4,Final:a_18") else "WRONG")(3)
-        #return redirect(url_for('stage_1'))
         name = request.form.get('name')
         number = request.form.get('number')
         description = request.form.get('description')
@@ -39,35 +38,36 @@ def chatpage():
     
 @app.route('/ragha',methods=['POST','GET'])
 def ragha():
+    global raksha_convo
+    global ragha_convo
     if request.method == 'POST':
         ragha_message = request.form.get('ragha_message')
-        global ragha_convo
-        ragha_convo += "Ragha: " + ragha_message + "\n" + "\n"
+        #ragha_convo += "Ragha: " + ragha_message + "\n" + "\n"
+        ragha_convo.append(ragha_message)
         return render_template('ragha.html', ragha_convo = ragha_convo, raksha_convo = raksha_convo)
     
     if request.method == 'GET':
         return render_template('ragha.html', ragha_convo = ragha_convo, raksha_convo = raksha_convo)
 
-    return render_template('ragha.html')
+    return render_template('ragha.html', ragha_convo = ragha_convo, raksha_convo = raksha_convo)
 
 
 
 @app.route('/raksha',methods=['POST','GET'])
 def raksha():
+    global raksha_convo
+    global ragha_convo
     if request.method == 'POST':
         raksha_message = request.form.get('raksha_message')
-        global raksha_convo
-        raksha_convo += "Raksha: " + raksha_message + "\n" + "\n"
-
-        return render_template('raksha.html', raksha_convo = raksha_convo, ragha_convo = ragha_convo)
+        raksha_convo.append(raksha_message)
+        return render_template('raksha.html', ragha_convo = ragha_convo, raksha_convo = raksha_convo)
     
     if request.method == 'GET':
         return render_template('raksha.html', ragha_convo = ragha_convo, raksha_convo = raksha_convo)
 
-    return render_template('raksha.html')
-    
+    return render_template('raksha.html', ragha_convo = ragha_convo, raksha_convo = raksha_convo)
+
 
 if __name__ == "__main__":
-    ragha_convo, raksha_convo = "", ""
+    ragha_convo, raksha_convo = [], []
     app.run(debug=True)
-    
