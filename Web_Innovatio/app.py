350b37024ed8e17842089a535104d3c353d40e5e
Ragha Rao <ragharao2001@gmail.com>
2023-07-31 04:10:14 +0800

    got the AI to work

--- Web_Innovatio/app.py
+++ Web_Innovatio/app.py
@@ -1,25 +1,29 @@
-from flask import Flask, render_template, request, redirect, url_for
-import random
-import sched, time
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+from model import gen_voice, check
 
 app = Flask(__name__)
 
 @app.route('/',methods=['POST','GET'])
 def contactpage():
-    if request.method == 'POST':
-        name = request.form.get('name')
-        number = request.form.get('number')
-        description = request.form.get('description')
-        mp3_path = request.form.get('mp3_path')
+    if  request.method == 'POST':
+        if request.form.get('name'):
+            name = request.form.get('name')
+            number = request.form.get('number')
+            description = request.form.get('description')
+            mp3_path = request.form.get('mp3_path')
 
-        contact = f"{mp3_path}"
+            contact = f"{mp3_path}"
 
-        print(name, number, description, mp3_path)
+            print("ptint everything: ",name, number, description, mp3_path)
 
-        return render_template('contactpage.html', name = name, contact = contact )
-    
-    if request.form.get('chat'):
-        return redirect(url_for('chatpage'))
+            return render_template('contactpage.html', name = name, contact = contact )
+        
+        else:
+            global friend
+            friend = request.form.get('friend_box')
+            print("this is what you typoed into the box: ", friend)
+            return render_template('ragha.html', friend = friend)
+
 
     return render_template('contactpage.html')
 
@@ -38,15 +42,30 @@ def chatpage():
     
 @app.route('/ragha',methods=['POST','GET'])
 def ragha():
+    if 'value' in request.form:
+        global path
+        if request.form['value']:
+            path = request.form['value']
+        # Process the value as per your requirements
+        # For example, you can store it in a database, perform some calculations, etc.
+        #print('Value received from client:', path)
+        print("initial set: ", path)
+        #gen_voice(path)
+
     global raksha_convo
     global ragha_convo
     if request.method == 'POST':
-        ragha_message = request.form.get('ragha_message')
-        #ragha_convo += "Ragha: " + ragha_message + "\n" + "\n"
-        ragha_convo.append(ragha_message)
-        return render_template('ragha.html', ragha_convo = ragha_convo, raksha_convo = raksha_convo)
+        if request.form.get('ragha_message'):
+            ragha_message = request.form.get('ragha_message')
+            #ragha_convo += "Ragha: " + ragha_message + "\n" + "\n"
+            ragha_convo.append(ragha_message)
+            return render_template('ragha.html', ragha_convo = ragha_convo, raksha_convo = raksha_convo)
     
     if request.method == 'GET':
+        if len(raksha_convo) != 0:
+            latest_message = raksha_convo[-1]
+            print("final check: ",path)
+            gen_voice(path, latest_message)
         return render_template('ragha.html', ragha_convo = ragha_convo, raksha_convo = raksha_convo)
 
     return render_template('ragha.html', ragha_convo = ragha_convo, raksha_convo = raksha_convo)
@@ -58,9 +77,10 @@ def raksha():
     global raksha_convo
     global ragha_convo
     if request.method == 'POST':
-        raksha_message = request.form.get('raksha_message')
-        raksha_convo.append(raksha_message)
-        return render_template('raksha.html', ragha_convo = ragha_convo, raksha_convo = raksha_convo)
+        if request.form.get('raksha_message'):
+            raksha_message = request.form.get('raksha_message')
+            raksha_convo.append(raksha_message)
+            return render_template('raksha.html', ragha_convo = ragha_convo, raksha_convo = raksha_convo)
     
     if request.method == 'GET':
         return render_template('raksha.html', ragha_convo = ragha_convo, raksha_convo = raksha_convo)
@@ -68,6 +88,15 @@ def raksha():
     return render_template('raksha.html', ragha_convo = ragha_convo, raksha_convo = raksha_convo)
 
 
+
+@app.route('/tryhtml', methods=['POST','GET'])
+def tryhtml():    
+    return render_template('try.html')
+
+
+
 if __name__ == "__main__":
+    path = ""
+    friend = ""
     ragha_convo, raksha_convo = [], []
     app.run(debug=True)
--- Web_Innovatio/model.py
+++ Web_Innovatio/model.py
@@ -1,16 +1,17 @@
 from elevenlabs import generate, play, set_api_key, clone
 import requests
-import pydub
-from pydub import AudioSegment
-# from pydub.playback import play
 
-def gen_voice():
+def check(abs_path):
+    print("22d834863846834683hkdwkdhwhkehr378")
+
+
+def gen_voice(abs_path, text):
     set_api_key("bfe30fd852173f67e2cf38bff7e5e002")
 
     # file = '\Users\Oyeda\Downloads\Dada_sample_audio.mp3'
     # open(file, 'r')
 
-    abs_path = "C:/Users/Oyeda/Downloads/Dada_sample_audio.mp3"
+    #abs_path = "C:/Users/Oyeda/Downloads/Dada_sample_audio.mp3"
     kash_sound_url = abs_path.replace('\\', '/')
 
     # r = requests.get(kash_sound_url, allow_redirects=True)
@@ -23,8 +24,7 @@ def gen_voice():
         files=[kash_sound_url]
     )
 
-    text = "Summary: The year is 2023, and technological advancements continue to shape the world."
     audio = generate(text=text, voice=voice)
 
-    with open('Gen_voice.mp3', 'wb') as f:
+    with open('templates/Gen_voice.mp3', 'wb') as f:
         f.write(audio)
